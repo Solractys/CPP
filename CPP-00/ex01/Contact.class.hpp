8128ea7ef9af7c505860c369e345f0e4309f7496
Solractys <csilva-s@student.42sp.org.br>
2026-07-07 22:17:20 -0300

    ...

---
+++ CPP-00/ex01/Contact.class.hpp
@@ -0,0 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Contact.class.hpp                                  :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: csilva-s <csilva-s@student.42sp.org.br>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/07/07 21:04:22 by csilva-s          #+#    #+#             */
+/*   Updated: 2026/07/07 21:08:53 by csilva-s         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+class Contact {
+	// o contact tem três dados
+	// primeiro nome
+	// sobrenome
+	// apelido
+	// numero de telefone
+	// segredo obscuro
+	public:
+	// Nos métodos publicos eu tenho que implementar funćões de get
+	// get Nome
+	// get sobrenome
+	// get apelido
+	// get numero de telefone
+	// get Segredo Obscuro
+};
+
