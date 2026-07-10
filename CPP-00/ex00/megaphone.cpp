4adcc0773caf8103c3fc6e50b2664c11f133a28a
Solractys <csilva-s@student.42sp.org.br>
2026-07-10 00:15:49 -0300

    ex00

--- CPP-00/ex00/megaphone.cpp
+++ CPP-00/ex00/megaphone.cpp
@@ -6,7 +6,7 @@
 /*   By: csilva-s <csilva-s@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/06/20 11:27:02 by csilva-s          #+#    #+#             */
-/*   Updated: 2026/06/30 20:28:31 by csilva-s         ###   ########.fr       */
+/*   Updated: 2026/07/10 00:15:02 by csilva-s         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,19 +17,17 @@
 
 int	main(int ac, char *av[])
 {
-	for (int i = 0; i < ac; i++)
-	{
-		for (int j = 0; av[i][j]; j++)
-		{
-			av[i][j] = toupper(av[i][j]);
-		}
-	}
 	if (ac == 1)
 	{
 		std::cout <<  "* LOUD AND UNBEARABLE FEEDBACK NOISE *" << std::endl;
 		return (0);
 	}
-	for (int i = 1; i < ac; i++)
-		std::cout << av[i];
+	for (int i = 0; i < ac; i++)
+	{
+		for (int j = 0; av[i][j]; j++)
+			av[i][j] = std::toupper(av[i][j]);
+		if (i != 0)
+			std::cout << av[i] << std::endl;
+	}
 	return (0);
 }
